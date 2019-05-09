82777ab6f710d0fef4b1a9fe064f50ded4dbc046
smutnywalen <kasiaziel98@gmail.com>
2019-05-09 21:44:09 +0200

    Styled the order summary, made it sticky, added grid layout and styled the form a bit.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+var coll = document.getElementsByClassName("company-info");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    event.preventDefault();
+    this.classList.toggle("collapsed");
+    var content = this.nextElementSibling;
+    content.classList.toggle("collapsed");
+  });
+}
